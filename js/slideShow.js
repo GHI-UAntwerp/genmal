ad5ac53f646c1f5a01658662b6874032162607e2
GHI-UAntwerp <69770285+GHI-UAntwerp@users.noreply.github.com>
2021-04-27 16:25:25 -0500

    Add files via upload

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,9 +1,9 @@
 if (window.matchMedia) {
-    var mq = window.matchMedia('(max-width:320px)'),
+    var mq = window.matchMedia('(max-width:560px)'),
         viewport = document.querySelector('meta[name=viewport]');
     mq.addListener(checkViewPort);
     checkViewPort(mq);
     }
 function checkViewPort(mq) {
-    mq.matches ? viewport.setAttribute('content', 'width=320') : viewport.setAttribute('content', 'width=device-width');
+    mq.matches ? viewport.setAttribute('content', 'width=560') : viewport.setAttribute('content', 'width=device-width');
     }
