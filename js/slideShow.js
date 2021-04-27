1bc69d21e4ab973ef8e8f3736a92fc891a65def3
GHI-UAntwerp <69770285+GHI-UAntwerp@users.noreply.github.com>
2021-04-27 13:31:58 -0500

    Beta -movil
    Corrects orientation on mobile version

--- js/slideShow.js
+++ js/slideShow.js
@@ -15,3 +15,13 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+if (window.matchMedia) {
+    var mq = window.matchMedia('(max-width:960px)'),
+        viewport = document.querySelector('meta[name=viewport]');
+    mq.addListener(checkViewPort);
+    checkViewPort(mq);
+    }
+function checkViewPort(mq) {
+    mq.matches ? viewport.setAttribute('content', 'width=960') : viewport.setAttribute('content', 'width=device-width');
+    }
