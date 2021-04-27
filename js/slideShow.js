d748f19156e77f2a08fd5fc50ea9f7d71a329b74
GHI-UAntwerp <69770285+GHI-UAntwerp@users.noreply.github.com>
2021-04-27 16:16:04 -0500

    Add files via upload

---
+++ js/slideShow.js
@@ -0,0 +1,9 @@
+if (window.matchMedia) {
+    var mq = window.matchMedia('(max-width:320px)'),
+        viewport = document.querySelector('meta[name=viewport]');
+    mq.addListener(checkViewPort);
+    checkViewPort(mq);
+    }
+function checkViewPort(mq) {
+    mq.matches ? viewport.setAttribute('content', 'width=960') : viewport.setAttribute('content', 'width=device-width');
+    }
