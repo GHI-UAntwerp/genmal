2b756ee79226fa5a95537a6ad9a8db18a2707289
GHI-UAntwerp <69770285+GHI-UAntwerp@users.noreply.github.com>
2021-06-24 07:01:35 -0500

    Solve mobile version
    Change from flexbox to grid layout

--- js/slideShow.js
+++ js/slideShow.js
@@ -7,3 +7,19 @@ if (window.matchMedia) {
 function checkViewPort(mq) {
     mq.matches ? viewport.setAttribute('content', 'width=560') : viewport.setAttribute('content', 'width=device-width');
     }
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
