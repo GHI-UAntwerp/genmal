92c2eb8de4788838c9f2c8a4c9d3e604a07e713c
GHI-UAntwerp <69770285+GHI-UAntwerp@users.noreply.github.com>
2021-12-15 13:35:48 -0500

    Delete js directory

--- js/javaShow.js
+++
@@ -1,9 +0,0 @@
-if (window.matchMedia) {
-    var mq = window.matchMedia('(max-width:960px)'),
-        viewport = document.querySelector('meta[name=viewport]');
-    mq.addListener(checkViewPort);
-    checkViewPort(mq);
-    }
-function checkViewPort(mq) {
-    mq.matches ? viewport.setAttribute('content', 'width=960') : viewport.setAttribute('content', 'width=device-width');
-    }
